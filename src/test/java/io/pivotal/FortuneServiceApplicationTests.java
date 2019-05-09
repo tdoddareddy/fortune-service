f89d24d3b619c09d514a26318466b4b5480f698c
tdoddareddy <tdoddareddy@pivotal.io>
2019-05-09 09:36:33 -0500

    Fixed issue with Spring-Cloud-Config-Client-2.1.1

--- src/test/java/io/pivotal/FortuneServiceApplicationTests.java
+++ src/test/java/io/pivotal/FortuneServiceApplicationTests.java
@@ -4,12 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = FortuneServiceApplication.class)
